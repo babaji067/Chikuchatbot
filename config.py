c30e5f69c5d744885c6960aa850853bbb68a29e0
tgwalehm-byte <tgwalehm@gmail.com>
2026-09-06 16:04:51 +0000

    ғɪʀsᴛ ᴄᴏᴍᴍɪᴛ !!

---
+++ config.py
@@ -0,0 +1,23 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID", "14050586"))
+API_HASH = getenv("API_HASH", "42a60d9c657b106370c79bb0a8ac560c")
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "8543495503"))
+SUPPORT_GRP = getenv("SUPPORT_GRP", "Lunaxbots")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "Lunaxbots")
+START_LOGGER = int(getenv("START_LOGGER", "-1004300650366"))
+EVENTS_LOGGER = int(getenv("EVENTS_LOGGER", "-1004300650366"))
+
+
+IMG = [
+    "https://files.catbox.moe/6qzh5y.jpg",
+    "https://files.catbox.moe/9fqgen.jpg",
+    "https://files.catbox.moe/et8fz6.jpg",
+    "https://files.catbox.moe/ow4v71.jpg",
+    "https://files.catbox.moe/nqzk5h.jpg"
+]
