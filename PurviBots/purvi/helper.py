4ed236f30c66a53c613f25419a2d1557dc0f0cec
ᴊαɕᴋ𝛅ꪮɴ.../ <riteshsingh12965@gmail.com>
2026-09-06 21:54:32 +0530

    Update helper.py

--- PurviBots/purvi/helper.py
+++ PurviBots/purvi/helper.py
@@ -24,16 +24,16 @@ HELP_ABOUT =f"""**❖ {PurviBots.mention} ᴀɴ ᴀɪ ʙᴀsᴇᴅ ᴄʜᴀᴛ-�
 ➥ ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀᴅᴍɪɴ ᴡɪᴛʜ ʙᴀsɪᴄ ʀɪɢʜᴛs.
 ➥ ᴧᴅᴅ ϻє ηᴏᴡ ʙᴧʙʏ ɪɴ ʏᴏᴜʀ ɢʀσᴜᴘs.
 ─────────────────────────
-❖ υᴘᴅᴧᴛєs ᴄʜᴧηηєʟ ➥ [ʙᴏᴛs](https://t.me/ZEEMUSICCOMPNY)
-❖ sυᴘᴘσʀᴛ ᴄʜᴧᴛ ➥ [ᴜᴘᴅᴀᴛᴇs](https://t.me/cewefesbuk)
+❖ υᴘᴅᴧᴛєs ᴄʜᴧηηєʟ ➥ [ʙᴏᴛs](https://t.me/Chhoti_bot_support)
+❖ sυᴘᴘσʀᴛ ᴄʜᴧᴛ ➥ [ᴜᴘᴅᴀᴛᴇs](https://t.me/messo_network)
 ─────────────────────────
-➤ ʙσᴛ sᴛᴧᴛυs & ϻσʀє ʙσᴛs - [ᴄʟɪᴄᴋ ʜєʀє](https://t.me/ZEEMUSICCOMPNY)
-➤ ᴘᴧɪᴅ ᴘʀσϻσᴛɪση :- [ᴄσηᴛᴧᴄᴛ ʜєʀє](https://t.me/vecag)**
+➤ ʙσᴛ sᴛᴧᴛυs & ϻσʀє ʙσᴛs - [ᴄʟɪᴄᴋ ʜєʀє](https://t.me/messo_network)
+➤ ᴘᴧɪᴅ ᴘʀσϻσᴛɪση :- [ᴄσηᴛᴧᴄᴛ ʜєʀє](https://t.me/BABA_OWNERR)**
 """
 
 HELP_MAIN =f"""**❖ ᴄʜσσsє ᴄᴧᴛєɢσʀʏ ғσʀ ʏσυ ɢєᴛ ʜєʟᴩ.
 
-» ɪꜰ ᴀɴʏ ɪssᴜᴇ ᴄᴏᴍᴇs - [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](t.me/cewefesbuk)**
+» ɪꜰ ᴀɴʏ ɪssᴜᴇ ᴄᴏᴍᴇs - [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](t.me/Chhoti_bot_support)**
 """
 
 
