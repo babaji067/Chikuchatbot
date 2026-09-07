58d7bc103971b211ac48f63007f5a22363163703
ᴊαɕᴋ𝛅ꪮɴ.../ <riteshsingh12965@gmail.com>
2026-09-07 18:25:56 +0530

    Update __main__.py

--- PurviBots/__main__.py
+++ PurviBots/__main__.py
@@ -42,7 +42,7 @@ async def send_startup_message():
 <b>• ᴘʏʀᴏɢʀᴀᴍ :-</b> <code>{pyrover}</code>
 <b>• ᴘʏᴛʜᴏɴ :-</b> <code>{pyver.split()[0]}</code>
 
-<b>» ᴘᴏᴡᴇʀᴇᴅ ʙʏ :- <a href="https://t.me/purvibots">ᴘᴜʀᴠɪ-ʙᴏᴛs</a></b>
+<b>» ᴘᴏᴡᴇʀᴇᴅ ʙʏ :- <a href="https://t.me/BE_ZACK">• 𝐉 𝚫 𝐂 𝐊 𝐒 𝐎 𝐍 ┇</a></b>
 """
 
         buttons = InlineKeyboardMarkup(
@@ -58,7 +58,7 @@ async def send_startup_message():
 
         await PurviBots.send_photo(
             chat_id=LOGGER_GROUP,
-            photo="https://files.catbox.moe/21ba26.jpg",
+            photo="https://anya-file-host.vercel.app/pjgej7xpy7",
             caption=text,
             reply_markup=buttons,        
         )
