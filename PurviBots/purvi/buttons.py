fa742b14315e5790b695672bebc6b13415487c64
🟢 B A B A   J I ⚡ <dabhimanyu40@gmail.com>
2026-09-17 10:08:39 +0530

    Update buttons.py

--- PurviBots/purvi/buttons.py
+++ PurviBots/purvi/buttons.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 HELP_BACK = [
 
     [
-        InlineKeyboardButton(text="⌯ 𝛅ᴜᴘᴘᴏʀᴛ ⌯", url=f"https://t.me/pikusupport"),
+        InlineKeyboardButton(text="⌯ 𝛅ᴜᴘᴘᴏʀᴛ ⌯", url=f"https://t.me/chhoti_bot_support"),
         InlineKeyboardButton(text="⌯ вᴧᴄᴋ ⌯", callback_data="back"),
     ],
 ]
@@ -40,14 +40,14 @@ HELP_BUTTON = [
 
     [
         InlineKeyboardButton(text="✙ ᴀᴅᴅ ᴍᴇ ✙", url=f"https://t.me/{PurviBots.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+ban_users"),
-        InlineKeyboardButton(text="⌯ 𝛅ᴜᴘᴘᴏʀᴛ ⌯", url=f"https://t.me/pikusupport"),
+        InlineKeyboardButton(text="⌯ 𝛅ᴜᴘᴘᴏʀᴛ ⌯", url=f"https://t.me/chhoti_bot_support"),
     ],
 ]
 
 ABOUT_BUTTON = [
     [
-        InlineKeyboardButton("⌯ 𝛅ᴜᴘᴘσʀᴛ ⌯", url="https://t.me/pikusupport"),
-        InlineKeyboardButton("⌯ ᴜᴘᴅᴧᴛє ⌯", url="https://t.me/pikubot18")
+        InlineKeyboardButton("⌯ 𝛅ᴜᴘᴘσʀᴛ ⌯", url="https://t.me/chhoti_bot_support"),
+        InlineKeyboardButton("⌯ ᴜᴘᴅᴧᴛє ⌯", url="https://t.me/messo_network")
     ],
     [
         InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data=f"back")
