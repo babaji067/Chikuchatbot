f2816cc7d665a11ad93398c569be130b098fdc8e
🟢 B A B A   J I ⚡ <dabhimanyu40@gmail.com>
2026-09-17 09:51:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ API_ID = int(getenv("API_ID", "14050586"))
 API_HASH = getenv("API_HASH", "42a60d9c657b106370c79bb0a8ac560c")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "8998874268"))
-SUPPORT_GRP = getenv("SUPPORT_GRP", "https://t.me/pikusupport")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "https://t.me/pikubot18")
-START_LOGGER = int(getenv("START_LOGGER", "-1004311700724"))
-EVENTS_LOGGER = int(getenv("EVENTS_LOGGER", "-1004311700724"))
+OWNER_ID = int(getenv("OWNER_ID", "7240937506"))
+SUPPORT_GRP = getenv("SUPPORT_GRP", "https://t.me/chhoti_bot_support")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "https://t.me/messo_network")
+START_LOGGER = int(getenv("START_LOGGER", "-1003937316543"))
+EVENTS_LOGGER = int(getenv("EVENTS_LOGGER", "-1003937316543"))
 
 
 IMG = [
